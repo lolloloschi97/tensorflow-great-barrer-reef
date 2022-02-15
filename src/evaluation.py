754164487a37482adbc72288dd01b053a1f1be54
lolloloschi97 <lorenzo.loschi@studio.unibo.it>
2022-02-15 15:19:57 +0100

    settings

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,7 +1,9 @@
 from tensorboard import program
+from hyper_param import *
 def launch_tensorboard(name_model):
-    tracking_address = "../src/logs/" + name_model # the path of your log file.
-    tb = program.TensorBoard()
-    tb.configure(argv=[None, '--logdir', tracking_address])
-    url = tb.launch()
-    print(f"Tensorflow listening on {url}")
+    if WITH_COLAB == False:
+        tracking_address = "../src/logs/" + name_model # the path of your log file.
+        tb = program.TensorBoard()
+        tb.configure(argv=[None, '--logdir', tracking_address])
+        url = tb.launch()
+        print(f"Tensorflow listening on {url}")
